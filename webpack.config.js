9fa586dfa1c8492365dd808ca69ce8aabe297ff4
Evgeniy Sedov <sedoff-online@yandex.ru>
2022-05-14 11:46:11 +0700

    feat: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 let mode = (process.env.NODE_ENV === 'production') ? 'production' : 'development';
 
 console.log(process.env.NODE_ENV);
@@ -5,4 +7,15 @@ console.log(mode);
 
 module.exports = {
   mode: mode,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+
+    ]
+
+  }
 }
