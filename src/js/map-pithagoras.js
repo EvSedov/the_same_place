e80dad6bc06640b01dd215060843ed960850c0d8
Evgeniy Sedov <sedoff-online@yandex.ru>
2022-10-09 12:29:57 +0700

    changes coordinates for pythagoras

--- src/js/map-pithagoras.js
+++ src/js/map-pithagoras.js
@@ -7,7 +7,7 @@ function init() {
   }, {
     searchControlProvider: 'yandex#search'
   });
-  var myPlacemark = new ymaps.Placemark([55.040435, 82.964573], {}, {
+  var myPlacemark = new ymaps.Placemark([55.040172, 82.963737], {}, {
     preset: 'islands#redIcon'
   });
   myMap.geoObjects.add(myPlacemark);
