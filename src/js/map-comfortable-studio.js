d87eb6f5c9de1e3e53da62936e8389466d078efd
Evgeniy Sedov <sedoff-online@yandex.ru>
2022-12-03 01:32:50 +0700

    adds page comfortable studio

---
+++ src/js/map-comfortable-studio.js
@@ -0,0 +1,16 @@
+var myMap;
+
+function init() {
+  myMap = new ymaps.Map("map", {
+    center: [55.011009, 82.931650],
+    zoom: 16
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+  var myPlacemark = new ymaps.Placemark([55.011009, 82.931650], { iconCaption: 'Студия № 1 в "Инских холмах"' }, {
+    preset: 'islands#yellowHomeIcon'
+  });
+  myMap.geoObjects.add(myPlacemark);
+};
+
+// ymaps.ready(init);
--- src/js/map.js
+++ src/js/map.js
@@ -2,7 +2,7 @@ var myMap;
 
 function init() {
   myMap = new ymaps.Map("map", {
-    center: [55.025143, 82.928453],
+    center: [55.039144, 82.954546],
     zoom: 11
   }, {
     searchControlProvider: 'yandex#search'
@@ -16,9 +16,13 @@ function init() {
   var myPlacemark3 = new ymaps.Placemark([55.077958, 82.924454], { iconCaption: 'ЖК Чкалов' }, {
     preset: 'islands#greenHomeIcon'
   });
+  var myPlacemark4 = new ymaps.Placemark([55.011009, 82.931650], { iconCaption: 'Студия № 1 в "Инских холмах"' }, {
+    preset: 'islands#yellowHomeIcon'
+  });
   myMap.geoObjects.add(myPlacemark1);
   myMap.geoObjects.add(myPlacemark2);
   myMap.geoObjects.add(myPlacemark3);
+  myMap.geoObjects.add(myPlacemark4);
 };
 
 // ymaps.ready(init);
