2137db72df858628300d5d01ca7d1c5be1466317
Evgeniy Sedov <sedoff-online@yandex.ru>
2023-05-12 22:42:15 +0700

    replaces photos for a quiet place

--- src/js/get-random-src.js
+++ src/js/get-random-src.js
@@ -1,4 +1,4 @@
-const imagesForQuietPlace = ['DSC_04_13', 'DSC_04_01', 'DSC_04_02', 'DSC_04_03', 'DSC_04_04', 'DSC_04_05', 'DSC_04_06', 'DSC_04_07', 'DSC_04_09', 'DSC_04_10', 'DSC_04_11', 'DSC_04_12'];
+const imagesForQuietPlace = ['DSC_04_08', 'DSC_04_01', 'DSC_04_02', 'DSC_04_03', 'DSC_04_04', 'DSC_04_05', 'DSC_04_06', 'DSC_04_07', 'DSC_04_09', 'DSC_04_10', 'DSC_04_11', 'DSC_04_12', 'DSC_04_13', 'DSC_04_14', 'DSC_04_15', 'DSC_04_16', 'DSC_04_17', 'DSC_04_18'];
 const imagesForComfortableStudio = ['DSC_05_06', 'DSC_05_01', 'DSC_05_02', 'DSC_05_03', 'DSC_05_04', 'DSC_05_05', 'DSC_05_07'];
 const imagesForIdealStudio = ['DSC_06_03', 'DSC_06_01', 'DSC_06_02', 'DSC_06_04', 'DSC_06_05', 'DSC_06_06'];
 const imagesForBrightStudio = ['DSC_07_04', 'DSC_07_01', 'DSC_07_02', 'DSC_07_03', 'DSC_07_05', 'DSC_07_06', 'DSC_07_07', 'DSC_07_08', 'DSC_07_09'];
